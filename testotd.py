069789e770be7a24665b8ee0357a48b74b6ec113
Chris Maurer <chris.maurer@tradingtechnologies.com>
2018-04-06 08:49:26 +0900

    Updates for order tag defaults test

--- testotd.py
+++ testotd.py
@@ -5,9 +5,10 @@ import ast
 
 oc_log = r'/Users/cmaurer/testlog.log'
 # oc_log = r'/var/log/debesys/OC_sgx.log'
-order_tags = ["account", "order_capacity", "sText2", "sText1", "PARTY_ROLE_CLIENT_ID",
+order_tags = ["account", "order_capacity", "sText2", "sText1", "sMemo", "PARTY_ROLE_CLIENT_ID",
               "PARTY_ROLE_INVESTMENT_DECISION_MAKER", "PARTY_ROLE_EXECUTING_TRADER", "text_tt", "order_origination",
-              "order_attribute_type"]
+              "order_attribute_type", "PARTY_ROLE_QUALIFIER_NATURAL_PERSON", "PARTY_ROLE_CLEARING_ACCOUNT",
+              "ORDER_ATTRIBUTE_TYPE_LIQUIDITY_PROVISION_ACTIVITY_ORDER"]
 
 
 def optmenu():
@@ -30,74 +31,71 @@ def parse_log_message(log_data):
     for msg in logmsg:
         # print msg
 
-        if len(msg.split("=")) == 1:
-            if msg != '}' and msg != '{':
-                print "msg:", msg
-                idx = logmsg.index(msg)
-                logmsg.pop(idx)
-
-        elif msg == "{":
-            bracketed_message = True
-            bracketed_message_list = []
-            # print "bracketed_message:", bracketed_message
+        if "=" not in msg:
+            if msg == "{":
+                bracketed_message = True
+                bracketed_message_list = []
+                # print "bracketed_message:", bracketed_message
+            elif bracketed_message and msg == '}':
+                if any(msg_type not in msg for msg_type in
+                       ["PARTY_ROLE_CLEARING_ACCOUNT", "PARTY_ROLE_CUSTOMER_ACCOUNT", "PARTY_ROLE_CLEARING_FIRM"]):
+                    message_list.append("|".join(bracketed_message_list))
+                bracketed_message = False
+                # print "bracketed_message:", bracketed_message
+                # print "bracketed_message_list:", bracketed_message_list
+            # elif bracketed_message and msg == "{":
+            #     bracketed_message = False
+            else:
+                pass
+                # # print "msg:", msg
+                # idx = logmsg.index(msg)
+                # logmsg.pop(idx)
 
         elif bracketed_message:
-            if msg == '}':
-                if "PARTY_ROLE_CLEARING_ACCOUNT" not in bracketed_message_list\
-                        and "PARTY_ROLE_CUSTOMER_ACCOUNT" not in bracketed_message_list:
-                    message_list.append(str(bracketed_message_list))
-                bracketed_message = False
-            # elif msg == '{':
-            #     pass
-            else:
-                bracketed_message_list.append(msg)
-            # print "bracketed_message:", bracketed_message
-            # print "bracketed_message_list:", bracketed_message_list
-
-        # elif msg == 'user_id':
-        #     security_desc_contains_spaces = False
-        #     message_list.append(' '.join(security_desc_list))
-
-        # if msg == '{':
-        #     bracketed_message_list = []
-        #     bracketed_message = True
-        # if msg == '}':
-        #     bracketed_message = False
-        #     bracketed_message_list.append(msg)
-        #     # msg = ' '.join(bracketed_message_list)
-        #     msg = bracketed_message_list[-2]
-        #
-        # if bracketed_message:
-        #     bracketed_message_list.append(msg)
-        #     if len(bracketed_message_list) in range(1, 5) and len(bracketed_message_list) % 3 == 0:
-        #         bracketed_message_list.append(',')
-
-        # elif security_desc_contains_spaces:
-        #     security_desc_list.append(msg)
+            bracketed_message_list.append(msg)
 
         else:
             message_list.append(msg)
 
-        # if 'security_desc' in msg:
-        #     security_desc_list = []
-        #     security_desc_contains_spaces = True
+        # print "bracketed_message:", bracketed_message
 
-    message_list_item = iter(message_list)
-    log_message_dict = dict(zip(message_list_item, message_list_item))
+
+    # message_list_item = iter(message_list)
+    # log_message_dict = dict(zip(message_list_item, message_list_item))
     for message_list_item in message_list:
         # print "message_list_item:", message_list_item
-        log_message_dict[message_list_item.split("=")[0]] = message_list_item.split("=")[1]
+        if "|" in message_list_item and "party" in message_list_item and "Company" not in message_list_item:
+            dict_update = message_list_item.split("|")
+            for item in dict_update:
+                if "party_role" in item:
+                    k = item.replace('party_role=', '')
+                if "party_id" in item:
+                    v = item.replace('party_id=', '')
+            log_message_dict[k] = v
+        elif "|" in message_list_item and "party" not in message_list_item and "Company" not in message_list_item:
+            if "v_string" in message_list_item:
+                dict_update = message_list_item.split("|")
+                for item in dict_update:
+                    if "name" in item:
+                        k = item.replace('name=', '')
+                    if "v_string" in item:
+                        v = item.replace('v_string=', '')
+                log_message_dict[k] = v
+            else:
+                log_message_dict[message_list_item.split("|")[0]] = message_list_item.split("|")[1]
+        else:
+            log_message_dict[message_list_item.split("=")[0]] = message_list_item.split("=")[1]
     #
-    print "log_message_dict:", log_message_dict
-    # return log_message_dict
+    # print "log_message_dict:", log_message_dict
+    return log_message_dict
 
 
 def verify_otd_data(log_message):
-    for element in log_message:
+    # print log_message.keys()
+    for element in log_message.iteritems():
         # print "element:", element
         for order_tag in order_tags:
-            if order_tag in element:
-                # ast.literal_eval(output)
+            if order_tag in str(element):
                 print "order_tag in element:", element
 
 
@@ -113,7 +111,7 @@ def parse_otd_data(order_id):
             print "#" * 20
             print line_list[0]
             print "#" * 20
-            print "\n\nline:", line, "\n\n"
+            # print "\n\nline:", line, "\n\n"
             if "secondary_cl_ord_id" in line:
                 log_message = [line_list[0], line_list[1].lstrip(' ')]
                 log_message.append(line_list[2])
@@ -139,9 +137,11 @@ def parse_otd_data(order_id):
             log_message.pop(message_index)
 
             # print "log_message:", log_message, "\n\n"
+            # for m in log_message:
+            #     print m
             verification = parse_log_message(log_message)
             # print "verification:", verification
-            # verify_otd_data(verification)
+            verify_otd_data(verification)
 
 # optmenu()
 parse_otd_data("65c581a7-96d4-43af-a2a3-dae163a6c56d")
