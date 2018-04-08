3caf467fa0fe5b97c87d6abf9f853f3b66f4914d
Chris Maurer <chris.maurer@tradingtechnologies.com>
2018-04-08 21:36:15 +0900

    Clean up code; start adding FIX validation

--- testotd.py
+++ testotd.py
@@ -1,7 +1,7 @@
 __author__ = 'cmaurer'
 
-from optparse import OptionParser
-import ast
+import argparse
+import uuid , binascii
 
 oc_log = r'/Users/cmaurer/testlog.log'
 # oc_log = r'/var/log/debesys/OC_hkex.log'
@@ -9,7 +9,7 @@ oc_log = r'/Users/cmaurer/testlog.log'
 order_tags={"account_code": "PARTY_ROLE_ACCOUNT_CODE",
             "clearing_account": "PARTY_ROLE_CLEARING_ACCOUNT",
             "clearing_firm_id": "PARTY_ROLE_CLEARING_FIRM",
-            "client": "Routing Member ID",
+            "client": "PARTY_ROLE_EXECUTING_FIRM",
             "client_id": "PARTY_ROLE_CLIENT_ID",
             "commodity_derivative_indicator": "ORDER_ATTRIBUTE_TYPE_RISK_REDUCTION_ORDER",
             "cti": "CTI Code",
@@ -29,12 +29,33 @@ order_tags={"account_code": "PARTY_ROLE_ACCOUNT_CODE",
             "trading_capacity": "order_capacity",
             "wce_house": "wce_house"}
 
+fix_order_type = {"35=D": "ExecutionReport",
+                  "35=F": "OrderCancelRequest",
+                  "35=G": "OrderCancelReplaceRequest"}
+
+fix_tags = {"114=": "Authorized Group ID",
+            "115=": "PARTY_ROLE_EXECUTING_FIRM",
+            "439=": "PARTY_ROLE_CLEARING_FIRM", #Clearing Firm ID, NYBOT House Number, WCE House Number
+            "440=": "PARTY_ROLE_CLEARING_ACCOUNT",
+            "9195=": "PARTY_ROLE_ACCOUNT_CODE",
+            "9700=": "order_origination",
+            "9701=": "order_capacity",
+            "9702=": "ORDER_ATTRIBUTE_TYPE_LIQUIDITY_PROVISION_ACTIVITY_ORDER",
+            "9703=": "ORDER_ATTRIBUTE_TYPE_RISK_REDUCTION_ORDER",
+            "9704=": "PARTY_ROLE_INVESTMENT_DECISION_MAKER",
+            "9705=": "PARTY_ROLE_EXECUTING_TRADER",
+            "9706=": "PARTY_ROLE_CLIENT_ID",
+            "9707=": "Mifid Id",
+            "9208=": "Cti"}
+
+
 def optmenu():
-    parser = OptionParser()
-    parser.add_option('-o', '--order', dest='order_id',
-                      help='order_id you want to evaluate', metavar='order_id')
-    optmenu, args = parser.parse_args()
-    parse_otd_data(optmenu.order_id)
+
+    parser = argparse.ArgumentParser(description='Get order number input from user.')
+    parser.add_argument('-o', action='store', metavar='order_id', help='enter order_id for analysis.', type=str)
+    order_id = parser.parse_args()
+
+    return order_id.o
 
 
 def parse_log_message(log_data):
@@ -67,9 +88,7 @@ def parse_log_message(log_data):
                 bracketed_message = True
                 bracketed_message_list = []
             elif bracketed_message and msg == '}':
-                if any(msg_type not in msg for msg_type in
-                       ["PARTY_ROLE_CLEARING_ACCOUNT", "PARTY_ROLE_CUSTOMER_ACCOUNT", "PARTY_ROLE_CLEARING_FIRM"]):
-                    message_list.append("|".join(bracketed_message_list))
+                message_list.append("&".join(bracketed_message_list))
                 bracketed_message = False
             else:
                 pass
@@ -87,17 +106,17 @@ def parse_log_message(log_data):
         message_list_item = message_list_item.replace('order_attribute_value=1', 'Yes')
         message_list_item = message_list_item.replace('order_attribute_value=0', 'No')
         # print message_list_item
-        if "|" in message_list_item and "party" in message_list_item and "Company" not in message_list_item:
-            dict_update = message_list_item.split("|")
+        if "&" in message_list_item and "party" in message_list_item:# and "Company" not in message_list_item:
+            dict_update = message_list_item.split("&")
             for item in dict_update:
                 if "party_role=" in item:
                     k = item.replace('party_role=', '')
                 if "party_id" in item:
                     v = item.replace('party_id=', '')
             log_message_dict[k] = v
-        elif "|" in message_list_item and "party" not in message_list_item and "Company" not in message_list_item:
+        elif "&" in message_list_item and "party" not in message_list_item:# and "Company" not in message_list_item:
             if "v_string" in message_list_item:
-                dict_update = message_list_item.split("|")
+                dict_update = message_list_item.split("&")
                 for item in dict_update:
                     if "name" in item:
                         k = item.replace('name=', '')
@@ -105,50 +124,66 @@ def parse_log_message(log_data):
                         v = item.replace('v_string=', '')
                 log_message_dict[k] = v
             else:
-                log_message_dict[message_list_item.split("|")[0].replace('\"', '')] = message_list_item.split("|")[1].replace('\"', '')
+                log_message_dict[message_list_item.split("&")[0].replace('\"', '')] = message_list_item.split("&")[1].replace('\"', '')
         else:
             log_message_dict[message_list_item.split("=")[0].replace('\"', '')] = message_list_item.split("=")[1].replace('\"', '')
 
     return [log_msg_type, log_message_dict]
 
 
-def parse_otd_data(order_id):
+def parse_fix_message(line):
+
+    log_message_dict = {}
+    verification_list = []
+    log_msg_type = fix_order_type[line[2]]
+    # u = binascii.hexlify(uuid.uuid4().bytes)
+    log_message_dict = line[2:]
+    # log_message_dict["order"] = line.split("\x01")
+
+    return [log_msg_type, log_message_dict]
+
+
+
+def parse_logfile_line(line):
+
+    if "FIX" in line:
+        log_message = line.encode("ascii").split("\x01")
+    else:
+        line_list = line.split(" | ")[5:]
+        if "order_submitter_inl.h" in line_list[0]:
+            line_list.pop(0)
+        if "secondary_cl_ord_id" in line:
+            log_message = [line_list[0], line_list[1].lstrip(' ')]
+            log_message.append(line_list[2])
+            log_message.append(line_list[3])
+            log_message.extend(line_list[4].split(" "))
+        elif "TradeCaptureReport" in line:
+            log_message = [line_list[0], line_list[1].lstrip(' ')]
+            log_message.append(line_list[2])
+            log_message.extend(line_list[3].split(" "))
+        else:
+            log_message = [line_list[0], line_list[1].lstrip(' ')]
+            log_message.extend(line_list[2].split(" "))
+
+    return log_message
+
+
+def verify_otd_data(order_id):
 
     verification_dict = {}
     verify_data_list = []
     er_counter = 0
     cr_counter = 0
+
     logfile = open(oc_log, 'r')
     for line in logfile.readlines():
-        verification_list = []
         if order_id in line:
-            line_list = line.split(" | ")[5:]
-            if "order_submitter_inl.h" in line_list[0]:
-                line_list.pop(0)
-            if "secondary_cl_ord_id" in line:
-                log_message = [line_list[0], line_list[1].lstrip(' ')]
-                log_message.append(line_list[2])
-                log_message.append(line_list[3])
-                log_message.extend(line_list[4].split(" "))
-            elif "TradeCaptureReport" in line:
-                log_message = [line_list[0], line_list[1].lstrip(' ')]
-                log_message.append(line_list[2])
-                log_message.extend(line_list[3].split(" "))
+            verification_list = []
+            log_message = parse_logfile_line(line)
+            if "FIX" in str(log_message):
+                verification = parse_fix_message(log_message)
             else:
-                log_message = [line_list[0], line_list[1].lstrip(' ')]
-                log_message.extend(line_list[2].split(" "))
-
-            # for instr_msg in log_message:
-            #     if "security_desc" in instr_msg:
-            #         message_index = log_message.index(instr_msg)
-            #         log_message.pop(message_index)
-            #         if "ice" in str(logfile):
-            #             log_message.pop(message_index)
-            #             log_message.pop(message_index)
-            #             log_message.pop(message_index)
-            #         break
-
-            verification = parse_log_message(log_message)
+                verification = parse_log_message(log_message)
 
             # Verify log message
             log_message_type = verification[0]
@@ -172,12 +207,18 @@ def parse_otd_data(order_id):
             verification_dict[log_message_type] = verification_list
             verify_data_list.append(log_message_type)
 
+    print "\n\n", "#"*30, "\nVerification Report:\n", "#"*30
     for i in range(0, len(verify_data_list)-1):
-        print "\n{0} and {1} match: {2}".format(verify_data_list[i], verify_data_list[i+1], verification_dict[verify_data_list[i]] == verification_dict[verify_data_list[i+1]])
+        print "{0}\n{1} and {2} match: {3}\n".format("-"*56, verify_data_list[i], verify_data_list[i+1], verification_dict[verify_data_list[i]] == verification_dict[verify_data_list[i+1]])
         if not verification_dict[verify_data_list[i]] == verification_dict[verify_data_list[i+1]]:
             for item in verification_dict[verify_data_list[i+1]]:
                 if item not in verification_dict[verify_data_list[i]]:
                     print "{0} contains: {1}".format(verify_data_list[i+1], item)
 
-# optmenu()
-parse_otd_data("65c581a7-96d4-43af-a2a3-dae163a6c56d")
+
+# order_id = optmenu()
+# order_id = "65c581a7-96d4-43af-a2a3-dae163a6c56d"
+# parse_otd_data(order_id)
+order_id="224005882790"
+verify_otd_data(order_id)
+#"65c581a7-96d4-43af-a2a3-dae163a6c56d"
