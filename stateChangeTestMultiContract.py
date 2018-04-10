834cb98f9febb474e1a1b329fac28401d6f1feea
Chris Maurer <chris.maurer@tradingtechnologies.com>
2018-04-10 09:17:48 +0900

    Updates for current test

--- stateChangeTestMultiContract.py
+++ stateChangeTestMultiContract.py
@@ -12,7 +12,7 @@ priceSrv = Manager().getPriceServer()
 prev_trading_status = None
 curr_trading_status = None
 pricey = None
-products = priceSession.getProducts(prodName='TOPIX', prodType=aenums.TT_PROD_FUTURE)
+products = priceSession.getProducts(prodName='TW', prodType=aenums.TT_PROD_FUTURE)
 run_now = True
 while run_now is True:
     prod = products[0]
@@ -30,7 +30,7 @@ while run_now is True:
     else:
         orderSession.deleteMyOrders()
         for product in products:
-            contracts = priceSession.getContracts(product)#, contractKeys="FUT_NK225M_1704")
+            contracts = priceSession.getContracts(product, contractKeys=["TWM18", "TWU18", "TWZ18", "TWH19"])
             for contract in contracts:
                 try:
                     for enum, price in priceSession.getPrices(contract).items():
@@ -47,11 +47,11 @@ while run_now is True:
                 else:
                     if pricey is not None:
                         for side in [aenums.TT_BUY, aenums.TT_SELL, aenums.TT_SELL]:
-                            ordqty = 60 if "SELL" in str(side) else 100
+                            ordqty = 60 if "SELL" in str(side) else 1000
                             orderParams = dict(order_qty=ordqty, buy_sell=side, order_action=aenums.TT_ORDER_ACTION_ADD, limit_prc=pricey, order_type=aenums.TT_LIMIT_ORDER, tif="GTD", srs=contract, exchange_clearing_account=custDefaults.exchange_clearing_account, free_text=custDefaults.free_text, acct_type=cppclient.AEnum_Account.TT_ACCT_AGENT_1)
                             if "SELL" in str(side):
                                 pricey = (cppclient.TTTick.PriceIntToInt(pricey, contract, -1))
-                                orderParams = dict(order_qty=ordqty, buy_sell=side, order_action=aenums.TT_ORDER_ACTION_ADD, limit_prc=pricey, order_type=aenums.TT_LIMIT_ORDER, tif="GTD", srs=contract, exchange_clearing_account=custDefaults.exchange_clearing_account, free_text=custDefaults.free_text, acct_type=cppclient.AEnum_Account.TT_ACCT_AGENT_1)
+                                orderParams = dict(order_qty=ordqty, buy_sell=side, order_action=aenums.TT_ORDER_ACTION_ADD, limit_prc=pricey, order_type=aenums.TT_MARKET_ORDER, tif="GTD", srs=contract, exchange_clearing_account=custDefaults.exchange_clearing_account, free_text=custDefaults.free_text, acct_type=cppclient.AEnum_Account.TT_ACCT_AGENT_1)
                             else:
                                 orderParams = dict(order_qty=ordqty, buy_sell=side, order_action=aenums.TT_ORDER_ACTION_ADD, limit_prc=pricey, order_type=aenums.TT_LIMIT_ORDER, tif="GTD", srs=contract, exchange_clearing_account=custDefaults.exchange_clearing_account, free_text=custDefaults.free_text, acct_type=cppclient.AEnum_Account.TT_ACCT_AGENT_1)
                             newOrder = TTAPIOrder()
@@ -61,7 +61,7 @@ while run_now is True:
                                 if "SRS_STATUS" in str(enum):
                                     if price.value != 2:
                                         depth_level = 1
-                                        while depth_level <= 3:
+                                        while depth_level <= 7:
                                             for side in [aenums.TT_SELL, aenums.TT_BUY]:
                                                 if "SELL" in str(side):
                                                     pricey = (cppclient.TTTick.PriceIntToInt(pricey, contract, -1))
@@ -76,6 +76,5 @@ while run_now is True:
                     #orderSession.deleteMyOrders()
                     # time.sleep(1)
                     # pyscreenshot.grab_to_file(r"C:\tt\screenshot_" + str(curr_trading_status) + "_" + "-".join([str(time.localtime()[3]), str(time.localtime()[4]), str(time.localtime()[5])]) + "_DELETE.png")
-            prev_trading_status = curr_trading_status
-
+                prev_trading_status = curr_trading_status
 
